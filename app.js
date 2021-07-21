b19a245d2ecc0be11df6faf37949f6e76078f161
Suryansh Chopra <suryanshchopra987@gmail.com>
2021-07-21 10:50:17 +0530

    App working

--- app.js
+++ app.js
@@ -2,28 +2,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Suryansh Chopra"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Suryansh Chopra"
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
 
 function getTranslationURL(text){
-    return serverURL + "?" + "text" + text
+    return serverURL + "?" + "text=" + text
 }
 
 function errorHandler(error) {
-    console.log("error occured", error)
-    alert("Something wrong with server: try again after some time")
+    console.log("error occured", error);
+    alert("Something wrong with server! try again after some time")
 }
 
 
 function clickHandler() {
-    // outputDiv.innerText = "符号 " +txtInput.value;
 
     var inputText = txtInput.value; // taking Input
 
     // calling server for processing
     fetch(getTranslationURL(inputText)) 
-    .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+            })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
